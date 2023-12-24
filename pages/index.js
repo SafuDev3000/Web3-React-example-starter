35c7fe7657093430306770fa211314c52ce8f716
SafuDev3000 <genograndino@gmail.com>
2023-12-24 13:10:55 -0800

    first comit

---
+++ pages/index.js
@@ -0,0 +1,55 @@
+import { useWeb3React } from "@web3-react/core"
+import { useEffect } from "react"
+import { injected } from "../components/wallet/connectors"
+
+
+const Moralis = require('moralis');
+// ES6 Minimized
+
+export default function Home() {
+  const { active, account, connector, library, activate, deactivate } = useWeb3React()
+ 
+  //connect wallet
+  // set variable to locall storage
+  // persist on device used
+  async function connect() {
+    try {
+      await activate(injected)
+      localStorage.setItem('isWalletConnected', true)
+    } catch (ex) {
+      console.log(ex)
+    }
+  }
+
+  async function disconnect() {
+    try {
+      deactivate()
+      localStorage.setItem('isWalletConnected', false)
+    } catch (ex) {
+      console.log(ex)
+    }
+  }
+
+  // if you see this in local storage then reconnect
+  useEffect(() => {
+    const connectWalletOnPageLoad = async () => {
+      if (localStorage?.getItem('isWalletConnected') === 'true') {
+        try {
+          await activate(injected)
+          localStorage.setItem('isWalletConnected', true)
+        } catch (ex) {
+          console.log(ex)
+        }
+      }
+    }
+    connectWalletOnPageLoad()
+  }, [])
+
+  return (
+    <div className="flex flex-col items-center justify-center">
+      <button onClick={connect} className="py-2 mt-20 mb-4 text-lg font-bold text-white rounded-lg w-56 bg-blue-600 hover:bg-blue-800">Connect to MetaMask</button>
+      {active ? <span>Connected with <b>{account}</b></span> : <span>Not connected</span>}
+      <button onClick={disconnect} className="py-2 mt-20 mb-4 text-lg font-bold text-white rounded-lg w-56 bg-blue-600 hover:bg-blue-800">Disconnect</button>
+    </div>
+  )
+}
